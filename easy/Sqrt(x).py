29846381adfb71bf4a42ea429e8a9d8d52d2d221
Syed Masood Hussain <hmasood3288@gmail.com>
2026-04-12 17:43:11 +0500

    Solved: 69. Sqrt(x)

---
+++ easy/Sqrt(x).py
@@ -0,0 +1,11 @@
+import math
+
+
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        result = int(math.sqrt(x))
+        return result
